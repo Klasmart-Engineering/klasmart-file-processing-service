f2e0f41935b7402c283b6ba408432198f346984c
Oscar Dovao <oscar.dovao@opencredo.com>
2022-03-16 12:00:39 +0000

    tf variable descriptions

--- deploy/tf/variables.tf
+++ deploy/tf/variables.tf
@@ -5,61 +5,74 @@ variable "region" {
 
 variable "lambda_function_name" {
   type = string
+  description = "lambda function name"
 }
 
 variable "lambda_function_image" {
   type = string
+  description = "lambda function container image"
 }
 
 variable "lambda_function_timeout" {
   type    = number
   default = 60
+  description = "lambda runtime timeout"
 }
 
 variable "lambda_function_memory" {
   type    = number
   default = 512
+  description = "lambda max memory available"
+}
+
+variable "lambda_function_retries" {
+  type    = number
+  default = 2
+  description = "Lambda number of retries for synchronous requests"
 }
 
 variable "inbound_bucket" {
   type = string
+  description = "the file upload bucket name"
 }
 
 variable "outbound_bucket" {
   type = string
+  description = "the processed file bucket name"
 }
 
 variable "enable_trigger" {
   type    = bool
   default = true
+  description = "Enable or disable the lambda SQS trigger"
 }
 
-variable "storage_driver" {
+variable "env_storage_driver" {
   type    = string
   default = "s3"
+  description = "env var for storage type"
 }
 
-variable "storage_accelerate" {
+variable "env_storage_accelerate" {
   type    = bool
   default = false
+  description = "env var for storage acceleration"
 }
 
-variable "log_level" {
+variable "env_log_level" {
   type    = string
   default = "debug"
+  description = "env var for logging level"
 }
 
-variable "log_std_out" {
+variable "env_log_std_out" {
   type    = bool
   default = true
+  description = "env var for logging to standard out"
 }
 
-variable "processors" {
+variable "env_processors" {
   type    = list(string)
   default = ["exif"]
-}
-
-variable "retries" {
-  type    = number
-  default = 2
+  description = "env var for the process types to execute on files"
 }
